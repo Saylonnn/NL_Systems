7b310218036d37ad180ad038c119c0f421b9ecf7
samue <samuel.raak@gmx.de>
2022-11-13 14:06:54 +0100

    polished fault tolerance

--- src/NebenlaeufigeSysteme/Aufgaben/Aufgabe4/Sensor.java
+++ src/NebenlaeufigeSysteme/Aufgaben/Aufgabe4/Sensor.java
@@ -83,8 +83,8 @@ public class Sensor implements SensorInterface  {
             //Abstand > 10 --> Höchstwarscheinlich ein Fehler oder eine Kurve (3er Kette prüfen)
             //Nicht 255, wenn counter 2 --> messwert übermitteln
             if (Math.abs(measurement - lastmeasurement) > 10 && measurement != 255 && big_abs_count == 2) {
-                ///WTF was macht das?
                 notifyObservers(measurement);
+                big_abs_count = 0;
                 sensorPrint("case 3");
             }
             //Großer Abstand Counter unter 2 -> hochzählen
@@ -95,12 +95,14 @@ public class Sensor implements SensorInterface  {
             //Zu nah an der Wand
             if (measurement == 255 && lastTransmittedValue <= 10) {
                 notifyObservers(0);
+                big_abs_count = 0;
                 sensorPrint("case 5");
             }
 
             // Nächstes Objekt zu weit entfernt
             if (measurement == 255 && thirdlastmeasurement < secondlastmeasurement && secondlastmeasurement < lastmeasurement) {
                 notifyObservers(80);
+                big_abs_count = 0;
                 sensorPrint("case 6");
             }
             //damit nicht durchgehend überprüft wird
