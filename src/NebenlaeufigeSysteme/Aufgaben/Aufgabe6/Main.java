7e4030901fe730ab9fe9ab3461985ffe300a65cb
saylonn <spacexd@gmx.de>
2022-11-22 10:53:08 +0100

    edid aufgabe 6

---
+++ src/NebenlaeufigeSysteme/Aufgaben/Aufgabe6/Main.java
@@ -0,0 +1,13 @@
+package NebenlaeufigeSysteme.Aufgaben.Aufgabe6;
+
+import NebenlaeufigeSysteme.Aufgaben.Aufgabe5_GUI.Controller;
+import NebenlaeufigeSysteme.Aufgaben.Aufgabe5_GUI.GUI;
+
+public class Main {
+    public static void main(String[] args) {
+        //if (args[0].equals("-GUI")&&a[1].equals("-GUI")) {
+        //    GUI s = new GUI();
+        //    Controller c = new Controller(s, s)
+        //}
+    }
+}
