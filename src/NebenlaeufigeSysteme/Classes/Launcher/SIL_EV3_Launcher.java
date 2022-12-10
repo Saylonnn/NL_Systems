4b3b120a36861497c4fb3b838f6a1e85ce6c2519
samue <samuel.raak@gmx.de>
2022-12-10 17:56:49 +0100

    impl nxt sensor

--- src/NebenlaeufigeSysteme/Classes/Launcher/SIL_EV3_Launcher.java
+++ src/NebenlaeufigeSysteme/Classes/Launcher/SIL_EV3_Launcher.java
@@ -1,6 +1,7 @@
 package NebenlaeufigeSysteme.Classes.Launcher;
 
 import NebenlaeufigeSysteme.Classes.Classes.Controller;
+import NebenlaeufigeSysteme.Classes.Classes.SilTest;
 import NebenlaeufigeSysteme.Classes_1_7.NXT.NXTMotor;
 import NebenlaeufigeSysteme.Classes_1_7.NXT.NXTSensor;
 import NebenlaeufigeSysteme.Interfaces.EngineInterface;
@@ -9,11 +10,9 @@ import NebenlaeufigeSysteme.Interfaces.SensorInterface;
 public class SIL_EV3_Launcher {
     public static void main(String[] args) {
 
-        SensorInterface sensor1 = new NXTSensor("fl");
-        SensorInterface sensor2 = new NXTSensor("fr");
-        SensorInterface sensor3 = new NXTSensor("bl");
-        SensorInterface sensor4 = new NXTSensor("br");
+        SensorInterface sil = new SilTest();
+
         EngineInterface engine = new NXTMotor();
-        Controller controller = new Controller(false, sensor1, sensor2, sensor3, sensor4, engine);
+        Controller controller = new Controller(false, sil, sil, sil, sil, engine);
     }
 }
